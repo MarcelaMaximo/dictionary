7cfda7f68ac0f769c4f945df07d28d0529859825
MarcelaMaximo <78973749+MarcelaMaximo@users.noreply.github.com>
2021-05-22 19:38:57 +0100

    search engine running

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Dictionary 
-      </header>
+      <div className="container">
+        <header className="App-header">
+          Dictionary 
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"> Coded by Marcela Máximo</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
