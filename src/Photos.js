56ef7a3eedaa4bfc05a5a81230cced5e20381689
MarcelaMaximo <78973749+MarcelaMaximo@users.noreply.github.com>
2021-05-29 00:08:16 +0100

    add rel="noreferrer"

--- src/Photos.js
+++ src/Photos.js
@@ -3,14 +3,13 @@ import "./Photos.css";
 
 export default function Photos (props) {
   if (props.photos) {
-    console.log(props.photos)
     return (
       <section className="Photos">
         {props.photos.map(function(photo, index){
           return(
             <div className="link" key={index}>
-              <a href={photo.src.original} target="_blank">
-                <img src={photo.src.landscape} className="img-fluid" rel="noreferrer"/>
+              <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <img src={photo.src.landscape} className="img-fluid" alt="pexels"/>
               </a>
             </div>
             )
